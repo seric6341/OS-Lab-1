7b9f25336efb89d797e471d88261a9ea457d5ca5
seric6341 <115032856+seric6341@users.noreply.github.com>
2022-12-12 09:50:14 -0500

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <lab1.h>
 
 char* readString(char *fileName){
   int s1_length = strlen(fileName);
-  int size = s1_length + 1;
+  int size = s1_length;
   char *s = calloc(size, sizeof(char));
   for(int i = 0; i < s1_length; i++){
+   
+    if s[i] == '\n':
+      s[i] = '\0';
     s[i] = fileName[i];
   }
+  printf("%s", s);
   return s;
   
     
@@ -17,24 +22,21 @@ char* readString(char *fileName){
 
 char* mysteryExplode(const char* str){
   int s1_length = strlen(str);
-  int size = s1_length + 1;
+  int size = s1_length;
   int explosion_size = size*(size+1)/2;
  
   char *s = calloc(explosion_size, sizeof(char));
   for(int i = 0; i < size; i++){
+   
+    if s[i] == '\n':
+      s[i] = '\0';
     strncat(s, str, i);
   }
   
-
+  printf("%s", s);
   return s;
 }
 
 
 
 
-int main(void) {
-
-  printf(readString("big"));
-  
-  printf(mysteryExplode("big"));
-}
